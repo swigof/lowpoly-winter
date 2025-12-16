48fb8120fda39b9c7152abdaeff2b737bac4a105
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-12-16 17:07:15 -0500

    add gameplay pausing

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -6,6 +6,7 @@ static var _first_capture := true
 
 func _on_play_button_pressed() -> void:
 	visible = false
+	get_tree().paused = false
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	_first_capture = false
 
@@ -37,7 +38,9 @@ func _input(event: InputEvent):
 func _process(_delta: float):
 	if not visible and Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE and not _first_capture:
 		visible = true
+		get_tree().paused = true
 	elif Input.is_action_just_pressed("pause"):
 		if not visible:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			visible = true
+			get_tree().paused = true
