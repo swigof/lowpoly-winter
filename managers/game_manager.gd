3946eaf1ce10caf9bfc5af1a58a6b3a4f2b9f352
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-12-19 10:05:52 -0500

    move window and pause logic to new game manager

---
+++ managers/game_manager.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var _first_capture := true
+var _menu: Control
+
+func _ready():
+	_menu = preload("res://scenes/menu/menu.tscn").instantiate()
+	_menu.visible = false
+	_menu.z_index = 1
+	add_child(_menu)
+
+func _input(event: InputEvent):
+	if not event is InputEventMouseButton or event.button_index != MOUSE_BUTTON_LEFT:
+		return
+	if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED and not get_tree().paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		_first_capture = false
+
+func _process(_delta: float):
+	if get_tree().paused:
+		return
+	var mouse_visible := Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE
+	if (mouse_visible and not _first_capture) or Input.is_action_just_pressed("pause"):
+		pause()
+
+func pause():
+	_menu.visible = true
+	get_tree().paused = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func unpause():
+	_menu.visible = false
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	_first_capture = false
--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,9 +1,5 @@
 extends Control
 
-# TODO Add actual pausing and move to a game/window manager
-
-static var _first_capture := true
-
 var sensitivity_label: Label
 var sensitivity_slider: Slider
 var volume_label: Label
@@ -28,10 +24,7 @@ func _update_volume():
 	volume_slider.value = SettingsManager.volume
 
 func _on_play_button_pressed() -> void:
-	visible = false
-	get_tree().paused = false
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	_first_capture = false
+	GameManager.unpause()
 
 func _on_settings_button_pressed() -> void:
 	$VBoxContainer/TopLevelVBox.visible = false
@@ -56,19 +49,3 @@ func _on_volume_slider_value_changed(value: float) -> void:
 	
 func _on_sens_slider_value_changed(value: float) -> void:
 	SettingsManager.change_sensitivity(value)
-
-func _input(event: InputEvent):
-	if (Input.mouse_mode != Input.MOUSE_MODE_CAPTURED) and event is InputEventMouseButton:
-		if not visible:
-			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-			_first_capture = false
-
-func _process(_delta: float):
-	if not visible and Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE and not _first_capture:
-		visible = true
-		get_tree().paused = true
-	elif Input.is_action_just_pressed("pause"):
-		if not visible:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			visible = true
-			get_tree().paused = true
