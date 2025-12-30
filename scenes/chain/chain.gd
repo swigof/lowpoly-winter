acf226955f5d87ffe14c1829f1c02820d1990983
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-12-30 15:29:02 -0500

    add grapple start sounds

--- scenes/chain/chain.gd
+++ scenes/chain/chain.gd
@@ -3,19 +3,32 @@ class_name Chain extends MultiMeshInstance3D
 const MESH_SCALE := Vector3.ONE * 0.05
 const SPACING := 0.15
 
+@export var hit_sound_player: AudioStreamPlayer3D
+@export var shoot_sound_player: AudioStreamPlayer3D
+
 var is_active: bool:
 	set(value):
 		visible = value
 		is_active = value
+		_first_frame = value
 var target: Vector3
+
+var _first_frame: bool
 
 func _process(_d):
 	if not is_active:
 		return
 	
+	if _first_frame:
+		hit_sound_player.global_position = target
+		hit_sound_player.play()
+		shoot_sound_player.play()
+		_first_frame = false
+	
 	var direction := target - global_position
 	var length := direction.length()
 	var unit_direction := direction / length
+	
 	var count: int = ceil(length / SPACING)
 	multimesh.instance_count = count
 	var offset := fmod(length, SPACING * 2)
