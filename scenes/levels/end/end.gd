0c655b5c569bcf7f4b903f94c6d00efbefba471a
SwigOf <33262954+SwigOf@users.noreply.github.com>
2025-12-30 20:07:35 -0500

    export missile start position

--- scenes/levels/end/end.gd
+++ scenes/levels/end/end.gd
@@ -3,6 +3,7 @@ extends Node3D
 @export var area: Area3D
 @export var missile: Node3D
 @export var missile_speed: float
+@export var missile_start_position: Vector3
 
 var _player: Player
 var _missile_forward_vector: Vector3
@@ -13,13 +14,13 @@ func _on_area_3d_body_entered(body: Node3D):
 		_player = body
 		_player.hook_max = 300
 		_missile_forward_vector = missile.global_basis.y
-		missile.position = Vector3(300, 100, 100)
+		missile.position = missile_start_position
 		_missile_is_moving = true
 
 func _on_area_3d_body_exited(body: Node3D):
 	if body == _player:
 		_player.hook_max = Player.HOOK_DISTANCE_DEFAULT
-		missile.position = Vector3(300, 100, 100)
+		missile.position = missile_start_position
 		_missile_is_moving = false
 
 func _process(delta: float):
