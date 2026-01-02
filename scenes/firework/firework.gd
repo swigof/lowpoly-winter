cb75b8e8228562253aee81eec8fbee3f9c98d145
SwigOf <33262954+SwigOf@users.noreply.github.com>
2026-01-01 19:59:38 -0500

    add more juice to firework

--- scenes/firework/firework.gd
+++ scenes/firework/firework.gd
@@ -1,7 +1,10 @@
-class_name Firework extends GPUParticles3D
+class_name Firework extends Node3D
+
+@export var particles: Array[GPUParticles3D]
 
 func _ready():
-	restart()
+	for particle in particles:
+		particle.restart()
 
 func _on_finished():
 	queue_free()
