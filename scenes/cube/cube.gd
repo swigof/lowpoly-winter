9246d2afc634cb7bf5e1573cc103758554630f74
SwigOf <33262954+SwigOf@users.noreply.github.com>
2026-01-03 21:29:27 -0500

    remove unused scenes and assets

--- scenes/cube/cube.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody3D
-
-func _ready():
-	var mesh = $MeshInstance3D.mesh as BoxMesh
-	var material = mesh.material as ShaderMaterial
-	material.set_shader_parameter("uv_scale", Vector2(scale.x, scale.x))
